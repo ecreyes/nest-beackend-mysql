7391ea56cb2f04a5f96263e6cead93f9fdc8803c
Eduardo Reyes <ereyesoyarzo@gmail.com>
2022-01-07 15:58:21 -0300

    redis configuration implemented

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,6 +1,7 @@
-import { Module } from '@nestjs/common'
+import { CacheModule, Module } from '@nestjs/common'
 import { JwtModule } from '@nestjs/jwt'
 
+import * as redisStore from 'cache-manager-redis-store'
 import { DatabaseModule } from '../database/database.module'
 
 @Module({
@@ -10,10 +11,17 @@ import { DatabaseModule } from '../database/database.module'
             secret: 'secret',
             signOptions: { expiresIn: '6h' },
         }),
+        CacheModule.register({
+            store: redisStore,
+            // Store-specific configuration:
+            host: 'redis',
+            port: 6379,
+        }),
     ],
     exports: [
         JwtModule,
         DatabaseModule,
+        CacheModule
     ],
 })
 export class SharedModule {}
